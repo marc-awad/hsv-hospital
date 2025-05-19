62dfe5e1f3f73cee47aafb7e397679e752962246
Marc AWAD <marc.awad@supdevinci-edu.fr>
2025-05-19 13:16:16 +0200

    Correcting router type import

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
+import { createRouter, createWebHistory } from "vue-router"
+import type { RouteRecordRaw } from "vue-router"
 
 import HomePage from "../components/Pages/HomePage.vue"
 import BookingPage from "../components/Pages/BookingPage.vue"
