70960591ab1bcfd1cd00c45af62e33d391b91c48
Marc AWAD <marc.awad@supdevinci-edu.fr>
2025-05-19 12:42:34 +0200

    Routing of the website

---
+++ src/router/index.ts
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
+
+import HomePage from "../components/Pages/HomePage.vue"
+import BookingPage from "../components/Pages/BookingPage.vue"
+import TrackingPage from "../components/Pages/TrackingPage.vue"
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomePage,
+  },
+  {
+    path: "/booking",
+    name: "Booking",
+    component: BookingPage,
+  },
+  {
+    path: "/tracking",
+    name: "Tracking",
+    component: TrackingPage,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
