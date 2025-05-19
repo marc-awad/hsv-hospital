1323d057fda41099838fcd59bc57a46d825b328b
Marc AWAD <marc.awad@supdevinci-edu.fr>
2025-05-19 13:19:55 +0200

    Firebase configuration and setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue"
+import "./style.css"
+import App from "./App.vue"
+import router from "./router"
+
+// Firestore testing
+// import { app as firebaseApp, analytics } from "../src/firebase/firebase"
+// import { getFirestore, collection, getDocs } from "firebase/firestore"
+
+
+// console.log("Firebase app:", firebaseApp)
+// console.log("Analytics:", analytics)
+
+// const db = getFirestore(firebaseApp)
+
+// getDocs(collection(db, "test-collection"))
+//   .then((snapshot) => {
+//     snapshot.forEach((doc) => {
+//       console.log(doc.id, " => ", doc.data())
+//     })
+//   })
+//   .catch((error) => {
+//     console.error("Firestore error:", error)
+//   })
 
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app.mount("#app")
