4eb611e4db5790e90cb5b990df4ae5e34f78ee4c
Marc AWAD <marc.awad@supdevinci-edu.fr>
2025-05-20 13:43:40 +0200

    main: homepage secondary text

---
+++ src/components/Atoms/SecondaryText.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const props = defineProps<{
+  shade?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900
+  sizeRem?: number
+  maxWidthPx?: number | string // ex: 600 (px) ou "600px"
+}>()
+
+const shade = props.shade ?? 500
+const sizeRem = props.sizeRem ?? 1
+
+const shadeMap: Record<number, string> = {
+  100: "text-gray-100",
+  200: "text-gray-200",
+  300: "text-gray-300",
+  400: "text-gray-400",
+  500: "text-gray-500",
+  600: "text-gray-600",
+  700: "text-gray-700",
+  800: "text-gray-800",
+  900: "text-gray-900",
+}
+
+const textColorClass = shadeMap[shade]
+const fontSizeStyle = { fontSize: `${sizeRem}rem` }
+
+const maxWidthStyle = props.maxWidthPx
+  ? {
+      maxWidth:
+        typeof props.maxWidthPx === "number"
+          ? `${props.maxWidthPx}px`
+          : props.maxWidthPx,
+    }
+  : {}
+</script>
+
+<template>
+  <p :class="textColorClass" :style="{ ...fontSizeStyle, ...maxWidthStyle }">
+    <slot />
+  </p>
+</template>
--- src/components/Pages/HomePage.vue
+++ src/components/Pages/HomePage.vue
@@ -1,9 +1,21 @@
 <template>
-  <div></div>
+  <div class="flex flex-col items-center justify-center text-center my-8 gap-y-4">
+    <PageTitle
+      title="Book your appointment in just a few clicks"
+      :sizeRem="3.25"
+      color="primary"
+    />
+    <SecondaryText :shade="500" :sizeRem="1.6" :maxWidthPx="800">
+      Access quality healthcare services with ease at HSV Hospital - your health
+      is our priority since 2017.
+    </SecondaryText>
+  </div>
 </template>
 
 <script>
-export default {}
+import PageTitle from "../Atoms/PageTitle.vue"
+import SecondaryText from "../Atoms/SecondaryText.vue"
+export default {
+  components: { PageTitle, SecondaryText },
+}
 </script>
-
-<style scoped></style>
