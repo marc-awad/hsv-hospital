7cc357672c40dc6c8856948ffa632fdfbb322a33
Marc AWAD <marc.awad@supdevinci-edu.fr>
2025-05-20 16:56:20 +0200

    main: starting tracking page

--- src/components/Pages/TrackingPage.vue
+++ src/components/Pages/TrackingPage.vue
@@ -1,9 +1,19 @@
 <template>
-  <div></div>
+  <div class="flex flex-col items-center text-center my-8 gap-y-4">
+    <PageTitle title="Track Your Appointment" :sizeRem="2.5" color="primary" />
+    <SecondaryText :shade="500" :sizeRem="1.2" :maxWidthPx="600"
+      >Easily find and manage your upcoming appointment by providing your
+      personal information below.</SecondaryText
+    >
+  </div>
 </template>
 
 <script>
-export default {}
+import PageTitle from "../Atoms/PageTitle.vue"
+import SecondaryText from "../Atoms/SecondaryText.vue"
+export default {
+  components: { PageTitle, SecondaryText },
+}
 </script>
 
 <style scoped></style>
