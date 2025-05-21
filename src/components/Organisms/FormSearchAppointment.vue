daff12e67795db8e5b058fb6c59f6e27424f8086
Marc AWAD <marc.awad@supdevinci-edu.fr>
2025-05-21 12:23:04 +0200

    style: improvement in UI/UX for the search appointment form

--- src/components/Organisms/FormSearchAppointment.vue
+++ src/components/Organisms/FormSearchAppointment.vue
@@ -10,21 +10,26 @@
       title="Search for your appointment"
       :sizeRem="1.3"
       color="primary"
+      class="text-left mb-8"
     />
-    <form action="">
+    <form action="" class="text-left">
       <Input
         label="Email"
         inputId="email"
         placeholder="your.email@example.com"
         class="mb-6"
       />
-      OR
+      <div class="flex items-center my-4">
+        <div class="flex-grow border-t border-gray-300"></div>
+        <span class="mx-4 text-gray-400 font-semibold">OR</span>
+        <div class="flex-grow border-t border-gray-300"></div>
+      </div>
       <Input
         label="Phone Number"
         inputId="phone"
         placeholder="+33 6 12 34 56 78"
       />
-      <Button type="submit" class="mt-6">Find Appointment</Button>
+      <Button type="submit" class="mt-6">Search</Button>
     </form>
   </div>
 </template>
