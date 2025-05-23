e0c348ac01447d4914c54e7cd448020d14f0fb9a
Marc AWAD <marc.awad@supdevinci-edu.fr>
2025-05-23 11:33:01 +0200

    still trying to fix vercel website...

--- api/email.cjs
+++ api/email.cjs
@@ -2,7 +2,7 @@ const { Resend } = require("resend")
 
 const resend = new Resend(process.env.RESEND_API_KEY || "")
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed, use POST" })
   }
